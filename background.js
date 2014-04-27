7ca6ac00630dcbf546cefc25d32b091e0d68fb46
Jeff Dalley <jeffdalley@gmail.com>
2014-04-27 03:58:42 -0400

    First commit.

---
+++ background.js
@@ -0,0 +1,32 @@
+// Background Stuff
+var bkg = chrome.extension.getBackgroundPage();
+
+// Intended to be used from a single context menu on selected text or hyperlink of a Twitch User.
+// It will construct a single user url for KBMOD's Multistream tool using the first/default layout.
+function openInMultiStream(info, tab) {
+  var msUrl;
+
+  if (info.selectionText) {
+    msUrl = "http://www.kbmod.com/multistream/view/?s0=" + info.selectionText + "&layout=1"
+  }
+  else if (info.linkUrl) {
+    // We're usually expecting the link to be the streamer's twitch url, but lets remove /profile in case.
+  	var twitchUser = info.linkUrl.replace('/profile', '');
+    twitchUser = twitchUser.substr(twitchUser.lastIndexOf('/') + 1);
+
+  	msUrl = "http://www.kbmod.com/multistream/view/?s0=" + twitchUser + "&layout=1"
+  }
+
+  chrome.tabs.create({
+    url: msUrl
+  })
+}
+
+// Add the context menu for selections and links.
+chrome.contextMenus.create({
+  title: "Open in MultiStream",
+  contexts: ["selection","link"],
+  onclick: openInMultiStream
+})
+
+
---
+++ kbmodhelper.js
@@ -0,0 +1,148 @@
+/*  
+  All KBMOD MultiStream code reference/influence and icon usage belongs to KBMOD - www.kbmod.com.
+
+  This tool is intended to provide a few shortcuts for those who still use the main Twitch TV 
+  website, but would like to quickly and easily switch to KBMOD MultiStream from either a single
+  channel (via hyperlink context menu), or up to 6 channels via the Extension button popup.
+
+  This is a rough version and I'll likely come back and implement enhancements soon.
+  @jeffdalley
+
+*/
+
+// Run as soon as the document's DOM is ready.
+document.addEventListener('DOMContentLoaded', function () {
+  // Set onclick functions for all add/remove/input elements.
+  // Have to do it this way due to extension rules against inline onclick code.
+  var addStreamerButtons = document.getElementsByClassName('addStreamerButton');
+  var removeStreamerButtons = document.getElementsByClassName('removeStreamerButton');
+  var streamInputs = document.getElementsByClassName('streamInput');
+
+  // Same number of elements, apply events in one pass.
+  var numAddButtons = addStreamerButtons.length;
+  for (var i = 0; i < numAddButtons; i++) {
+  	addStreamerButtons[i].onclick = setCurrentStreamerName;
+  	removeStreamerButtons[i].onclick = clearCurrentStreamerName;
+  	streamInputs[i].onkeyup = saveStreams;
+
+  	// Load saved values if any...
+    loadStreamValue(i);
+  }
+
+  // Set onclick for Open MultiStream button
+  var openMultiStreamButton = document.getElementById('openMsButton');
+  openMultiStreamButton.onclick = openMultiStream;
+});
+
+// Sets a given Streamer username from the current tab:
+// Only intended to work if current tab is on a streamer's twitch page or profile.
+function setCurrentStreamerName(e) {
+  var streamId = e.target.id.slice(-1);
+  var streamInput = document.getElementById("stream" + streamId);
+  chrome.tabs.query({ active: true, lastFocusedWindow: true},
+    function(arrayOfTabs) {
+	  var currTab = arrayOfTabs[0];
+
+	  var twitchUser = currTab.url.replace('/profile', '');
+      twitchUser = twitchUser.substr(twitchUser.lastIndexOf('/') + 1);
+      streamInput.value = twitchUser;
+
+      saveStreams(e);
+    }
+  );
+}
+
+// Save a given streamer username to storage.
+function saveStreams(e) {
+  var streamId = "stream" + e.target.id.slice(-1);
+  var streamInput = document.getElementById(streamId);
+
+  var obj = {};
+  obj[streamId] = streamInput.value;
+  chrome.storage.sync.set(obj, function () {
+    return false;
+  });
+}
+
+// Retrieve and set a given streamer username from storage.
+function loadStreamValue(streamIndex) {
+  var streamId = "stream" + streamIndex;
+  var streamInput = document.getElementById(streamId);
+  
+  chrome.storage.sync.get(streamId, function (data) {
+    if (data[streamId]) {
+      streamInput.value = data[streamId];
+    }
+  })
+}
+
+// Clear a given streamer username input box.
+function clearCurrentStreamerName(e) {
+  var streamToClear = document.getElementById("stream" + e.target.id.slice(-1));
+  streamToClear.value = '';
+  saveStreams(e);
+}
+
+// Construct a KBMOD MultiStream link from filled-in streamer usernames.
+function openMultiStream() {
+  var msUrl = "http://www.kbmod.com/multistream/view/?";
+  var msLayout = "&layout=";
+
+  var inputs = document.getElementsByClassName('streamInput');
+  var numInputs = inputs.length;
+  var twitchUsers = [];
+
+  for (var i = 0; i < numInputs; i++) {
+  	if (inputs[i].value) {
+  		twitchUsers.push(inputs[i].value);
+  	}
+  }  
+
+  var numUsers = twitchUsers.length;
+  for (var i = 0; i < numUsers; i++) {
+  	var prefix = "&s";
+  	if (i == 0) {
+  		prefix = "s";
+  	}
+  	msUrl += prefix + i + "=" + twitchUsers[i];
+  }
+
+  /*
+    Default/1st layout id per number of streamers:
+    1: 0
+    2: 3
+    3: 6
+    4: 9
+    5: 12
+    6: 15
+  */
+  var layoutId;
+  switch (numUsers) { 
+    case 1:
+      layoutId = 0;
+      break;
+    case 2:
+      layoutId = 3;
+      break;
+    case 3:
+  	  layoutId = 6;
+  	  break;
+    case 4:
+      layoutId = 9;
+  	  break;
+    case 5:
+  	  layoutId = 12;
+  	  break;
+    case 6:
+      layoutId = 15;
+  	  break;
+  }
+
+  msUrl += msLayout + layoutId;
+
+  // Launch a new chrome tab with the newly constructed KBMOD MS Url.
+  chrome.tabs.create({
+    url: msUrl
+  })
+}
+
