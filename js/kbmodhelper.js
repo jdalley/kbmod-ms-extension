48d8eb77608ff0f6065dca46694f9f86e80a8fc6
Jeff Dalley <jeffdalley@gmail.com>
2016-03-06 00:35:42 -0500

    removed stupid things

--- js/kbmodhelper.js
+++ js/kbmodhelper.js
@@ -4,12 +4,8 @@
   This tool is intended to provide a few shortcuts for those who still use the main Twitch TV 
   website, but would like to quickly and easily switch to KBMOD MultiStream from either a single
   channel (via hyperlink context menu), or up to 6 channels via the Extension button popup.
-
-  This is a rough version and I'll likely come back and implement enhancements soon.
+  
   @jeffdalley
-
-  Note: Used vanilla Javascript quite a bit for funsies.
-
 */
 
 // Run as soon as the document's DOM is ready.
