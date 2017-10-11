0e7e86ca2728d0ac36e19824cf0f068bd845846a
Jeff Dalley <jeffdalley@gmail.com>
2017-10-10 20:47:57 -0500

    Updated for multistream url change

--- js/background.js
+++ js/background.js
@@ -4,18 +4,20 @@ var bkg = chrome.extension.getBackgroundPage();
 // Intended to be used from a single context menu on selected text or hyperlink of a Twitch User.
 // It will construct a single user url for KBMOD's Multistream tool using the first/default layout.
 function openInMultiStream(info, tab) {
-  var msUrl;
+  var msUrl = "https://multistre.am/";
 
   if (info.selectionText) {
-    msUrl = "http://www.kbmod.com/multistream/view/?s0=" + info.selectionText + "&layout=1"
+    msUrl += info.selectionText;
   }
   else if (info.linkUrl) {
     // We're usually expecting the link to be the streamer's twitch url, but lets remove /profile in case.
   	var twitchUser = info.linkUrl.replace('/profile', '');
     twitchUser = twitchUser.substr(twitchUser.lastIndexOf('/') + 1);
 
-  	msUrl = "http://www.kbmod.com/multistream/view/?s0=" + twitchUser + "&layout=1"
+  	msUrl += twitchUser;
   }
+    
+  msUrl += "/layout0";
 
   chrome.tabs.create({
     url: msUrl
